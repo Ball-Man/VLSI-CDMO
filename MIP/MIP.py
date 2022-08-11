322c685659cbd0abb9cd397018c4a17d6b9589d8
FaediMichele <work.faedi.michele@gmail.com>
2022-08-11 23:04:47 +0200

    Added the column and row symmetry breaking. They make the program run veeery slow in fact its disabled and only for the no rotation version but matematically it work(usefull to discuss in the exam I think)

--- MIP/MIP.py
+++ MIP/MIP.py
@@ -57,7 +57,7 @@ def lex_less(arr1, arr2, dom1, dom2, model, key):
             [1 - dom1[k][0] + dom2[k][1] for k in range(0,i)] + #  B = -1, Mk = 1 + max(-1*dom1[k][0], -1*dom1[k][1]) + max(dom2[k])   ->   1 - dom1[k][0] + dom2[k][1]
             [dom1[i][1]], model, f"{key}_{i}")
     
-def linear_max(arr, dom, model, key, vtype=GRB.INTEGER):
+def linear_max(arr, dom, y, model, key):
     ''' Max constraint.
         
         linear_max([x1, x2, ..., xk], [(l_1,u_1), (l_2,u_2), ..., (l_k,u_k)], model, key)
@@ -67,35 +67,38 @@ def linear_max(arr, dom, model, key, vtype=GRB.INTEGER):
     #   y >= xi for i in 1..k
     #   y <= xi + (umax - l_i)*(1-b_i) where umax = max(u_1, u_2, ..., u_k) and b_i = 1 -> xi = max(x1, x2, ..., xk)
 
-    max_var = model.addVar(vtype=vtype, name=f"{key}")
     b = []
     u_max = max(dom[i][1] for i in range(len(arr)))
     for i in range(len(arr)):
         b.append(model.addVar(vtype=GRB.BINARY, name=f"{key}_b_{i}"))
-        model.addConstr(max_var >= arr[i], name=f"{key}_max1_{i}")
-        model.addConstr(max_var <= arr[i] + (u_max - dom[i][0])*(1-b[i]), name=f"{key}_max2_{i}")
+        model.addConstr(y >= arr[i], name=f"{key}_max1_{i}")
+        model.addConstr(y <= arr[i] + (u_max - dom[i][0])*(1-b[i]), name=f"{key}_max2_{i}")
 
     model.addConstr(sum(b) == 1, f"{key}_sum_b")
-    return max_var
+    return y
 
 
 
-def problem_no_rotation(width, n, circuits):
+def problem_no_rotation(width, n, circuits, sort_column_row = False):
     m = gp.Model("mip1")
-    m.setParam('TimeLimit', 5*60)
+    #m.setParam('TimeLimit', 5*60)
     min_height = sum([circuits[i][0]*circuits[i][1] for i in range(n)]) // width
     max_height = max(max([circuits[i][0] for i in range(n)]), sum([circuits[i][0] for i in range(n)]))
+    print(f"Best height: {min_height}")
 
     x = []
     y = []
     for i in range(n):
         x.append(m.addVar(vtype=GRB.INTEGER, name=f"x_{i}"))
         y.append(m.addVar(vtype=GRB.INTEGER, name=f"y_{i}"))
-
         m.addConstr(x[i] <= width - circuits[i][0], f"width_{i}")
-    print(f"Best height: {min_height}")
-
-    height = linear_max([y[i] + circuits[i][1] for i in range(n)], [(0, max_height) for _ in range(n)], m, "height")
+    
+    height = m.addVar(vtype=GRB.INTEGER, name="height")
+    linear_max(
+        [y[i] + circuits[i][1] for i in range(n)],
+        [(0, max_height) for _ in range(n)],
+        height, m, "height")
+        
     m.addConstr(height >= min_height, name="optimal_solution")
     
     # Non overlap
@@ -138,13 +141,65 @@ def problem_no_rotation(width, n, circuits):
         for j in range(i+1,n):
             if circuits[i][0] == circuits[j][0] and circuits[i][1] == circuits[j][1]:
                 lex_less([x[i], y[i]], [x[j], y[j]], [(0,width), (0,max_height)], [(0,width), (0,max_height)], m, f"equal_{i}_{j}")
+
+
+    
+    if sort_column_row:
+        # Same width and same column sort by size in descending order 
+        # w[i] = w[j] /\ h[i] < h[j] |= (x[i] = x[j] /\ h[i] = y[j] - y[i]) -> y[j] < y[i]
+        for i in range(n):
+            for j in range(i+1, n):
+                if circuits[i][0] == circuits[j][0]:
+                    (h,k) = (i,j) if circuits[i][1] < circuits[j][1] else (j,i) # if h[i] > h[j] swap i with j
+
+                    reify_or(
+                        [
+                            x[h] - x[k] + 1,                        # x[i] <= x[j] - 1
+                            x[k] - x[h] + 1,                        # x[i] >= x[j] + 1
+                            y[k] - y[h] - circuits[h][1] + 1,       # h[i] <= y[j] - y[i] - 1
+                            y[h] - y[k] + circuits[h][1] + 1,       # h[i] >= y[j] - y[i] + 1
+                            y[k] - y[h] + 1                         # y[j] <= y[i] - 1
+                        ],
+                        [
+                            1 + width,                              # 1 + max(1*0, 1*width) + max(-1*0, -1*width)
+                            1 + width,                              # 1 + max(1*0, 1*width) + max(-1*0, -1*width)
+                            - circuits[h][1] + 1 + max_height,      # - h[i] + 1 + max(1*0, 1*max_height) + max(-1*0, -1*max_height)
+                            circuits[h][1] + 1 + max_height,        # h[i] + 1 + max(1*0, 1*max_height) + max(-1*0, -1*max_height)
+                            1 + max_height                          # 1 + max(1*0, 1*max_height) + max(-1*0, -1*max_height)
+                        ], m, f"same_column_{h}_{k}"
+                    )
+
+
+        # Same height and same row in ascending order 
+        # h[i] = h[j] /\ w[i] < w[j] |= (y[i] = y[j] /\ w[i] = x[j] - x[i]) -> x[j] < x[i]
+        for i in range(n):
+            for j in range(i+1, n):
+                if circuits[i][1] == circuits[j][1]:
+                    (h,k) = (i,j) if circuits[i][0] < circuits[j][0] else (j,i) # if h[i] > h[j] swap i with j
+
+                    reify_or(
+                        [
+                            y[h] - y[k] + 1,                        # y[i] <= y[j] - 1
+                            y[k] - y[h] + 1,                        # y[i] >= y[j] + 1
+                            x[k] - x[h] - circuits[h][0] + 1,       # w[i] <= x[j] - x[i] - 1
+                            x[h] - x[k] + circuits[h][0] + 1,       # w[i] >= x[j] - x[i] + 1
+                            x[k] - x[h] + 1                         # x[j] <= x[i] - 1
+                        ],
+                        [
+                            1 + max_height,                         # 1 + max(1*0, 1*max_height) + max(-1*0, -1*max_height)
+                            1 + max_height,                         # 1 + max(1*0, 1*max_height) + max(-1*0, -1*max_height)
+                            - circuits[h][0] + 1 + width,           # - w[i] + 1 + max(1*0, 1*width) + max(-1*0, -1*width)
+                            circuits[h][0] + 1 + width,             # w[i] + 1 + max(1*0, 1*width) + max(-1*0, -1*width)
+                            1 + width                               # 1 + max(1*0, 1*width) + max(-1*0, -1*width)
+                        ], m, f"same_column_{h}_{k}"
+                    )
     
     m.setObjective(height, GRB.MINIMIZE)
     m.optimize()
     result = json.loads(m.getJSONSolution())
 
-    rect = [(circuits[i][0], circuits[i][1], int(x[i].X), int(y[i].X)) for i in range(n)]
-    return {"result": {"width": width, "height": int(height.X), "rect": rect},
+    rect = [(circuits[i][0], circuits[i][1], round(x[i].X), round(y[i].X)) for i in range(n)]
+    return {"result": {"width": width, "height": round(height.X), "rect": rect},
             "statistics": result['SolutionInfo'],
             "status": result['SolutionInfo']["Status"]}
 
@@ -254,7 +309,7 @@ def main():
 
         print(f'solving instance: {pt.basename(instance_file)}')
         print(instance_data)
-        result = problem_rotation(**instance_data)
+        result = problem_no_rotation(**instance_data)
 
         dump_statistics(result["statistics"], result["status"])
         print()
