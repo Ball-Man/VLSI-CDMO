d6b4e438005a6070e4b1839a9b6e09870aa6fd2b
Jyllinge <110840200+Jyllinge@users.noreply.github.com>
2022-09-05 13:49:09 +0200

    Update SAT_model.py
    Removed/modified comments

--- alfieri_faedi_mistri/SAT/src/SAT_model.py
+++ alfieri_faedi_mistri/SAT/src/SAT_model.py
@@ -32,11 +32,11 @@ def at_most_one(bool_vars, name):
 def equal_vars(var1,var2):  #boolean variables are equal iff they are equivalent
     return And(Or(Not(var1),var2),Or(Not(var2),var1))
 
-##def lex_order(listvar1,listvar2):   #ORDINE LESSICOGRAFICO (NAIVE ENCODING)
+##def lex_order(listvar1,listvar2):   #Lexicographic order (NAIVE ENCODING)
 ##    constraints=[]
 ##    n=len(listvar1)
 ##    constraints.append(Or(Not(listvar1[0]),listvar2[0])) #first element of first list is ""<="" of first element of second list
-##    for i in range(1,n):     #il constraints "intero" è troppo grande, si può provare a giocare con questo range
+##    for i in range(1,n):     #The "whole" constraint is too large, we can adapt this range
 ##        #constraints.append(Or(Not(And([equal_vars(listvar1[j],listvar2[j]) for j in range(i)])), Or(listvar1[i],Not(listvar2[i]))))
 ##        #constraints.append(Implies(And([equal_vars(listvar1[k],listvar2[k]) for k in range(i)]),Implies(listvar1[i],listvar2[i])))
 ##        constraints.append(Or(Not(And([equal_vars(listvar1[k],listvar2[k]) for k in range(i)])),Or(Not(listvar1[i]),listvar2[i])))
@@ -44,15 +44,15 @@ def equal_vars(var1,var2):  #boolean variables are equal iff they are equivalent
 
 def lex_order(listvar1,listvar2,name, func = math.sqrt):  #lex_order_CSE
     constraints=[]
-    n = len(listvar1)           #Anche qui si può provare a prendere f(n)<n per alleggerire il numero di constraints
+    n = len(listvar1)
     s = [Bool(f's_{name}_{i}') for i in range(n-1)]
     constraints.append(Or(Not(listvar2[0]),listvar1[0])) #lex_geq
     #constraints.append(Or(Not(listvar1[0]),listvar2[0]))   #lex_lesseq
     constraints.append(equal_vars(s[0],equal_vars(listvar1[0],listvar2[0])))
-    #for i in range(n-2):   #ordine lessicografico completo
-    for i in range(int(func(n)) - 2):
+    #for i in range(n-2):   #complete lexicographic order
+    for i in range(int(func(n)) - 2):   
         constraints.append(equal_vars(s[i+1], And(s[i], equal_vars(listvar1[i+1],listvar2[i+1]))))
-    #for i in range(n-1):   #ordine lessicografico completo
+    #for i in range(n-1):   #complete lexicographic order
     for i in range(int(func(n)) - 1):
         constraints.append(Or(Not(s[i]),(Or(Not(listvar2[i+1]),listvar1[i+1])))) #lex_geq
         #constraints.append(Or(Not(s[i]),(Or(Not(listvar1[i+1]),listvar2[i+1])))) #lex_lesseq
@@ -93,42 +93,23 @@ def sat_vlsi(width, nofrectangles, dimensions, min_height, timeout=300000): #dim
     starting_time=time.time()
     print('generating solver:')
     
-    for k in range(nofrectangles):  #ogni rettangolo ha almeno un'origine
-        s.add(exactly_one(flatten(X[k]), f"origin_{k}"))
-        # s.add(at_least_one(flatten(X[k])))  #per avere meno clauses posso mettere anche che ogni rettangolo ha almeno un'origine:
-                                            #alla peggio puoi ottenere come soluzione un rettangolo con circuiti duplicati,
-                                            #e quando costruisci fisicamente il chip scegli dove piazzare i circuiti in una qualsiasi delle posizioni restituite dalla soluzione
-
-    
+    for k in range(nofrectangles):  #Each circuit has an origin
+        #s.add(exactly_one(flatten(X[k]), f"origin_{k}"))
+        s.add(at_least_one(flatten(X[k])))  #In order to obtain a smaller number of clauses we impose at_least_one instead of exactly_one, see the report
+                        
     #constraints no-overlap:
-##    for k in range(nofrectangles):
-##        for i in range(width - dimensions[k][0] + 1):
-##            for j in range(min_height - dimensions[k][1] + 1): 
-##                for k1 in range(k+1, nofrectangles): #prima era range(nofrectangles), con if k != k1, ma così si tolgono un po' di implied constraints (modello è più piccolo)
-##                    #if k != k1:
-##                        for i1 in range(max(i-dimensions[k1][0]+1,0), min(i+dimensions[k][0], width - dimensions[k1][0] +1)):
-##                            for j1 in range(max(j-dimensions[k1][1]+1,0), min(j+dimensions[k][1], min_height - dimensions[k1][1] +1)):     #si dovrebbe capire graficamente                                                                                       
-##                                s.add(Or(Not(X[k][j][i]), Not(X[k1][j1][i1])))
-
     for k in range(nofrectangles-1):
         for i in range(width - dimensions[k][0] + 1):
             for j in range(min_height - dimensions[k][1] + 1):
-                for k1 in range(k+1, nofrectangles): #prima era range(nofrectangles), con if k != k1, ma così si tolgono un po' di implied constraints (modello è più piccolo)
-                    #if k != k1:
+                for k1 in range(k+1, nofrectangles): 
                         for i1 in range(max(i-dimensions[k1][0]+1,0), min(i+dimensions[k][0], width - dimensions[k1][0] +1)):
-                            for j1 in range(max(j-dimensions[k1][1]+1,0), min(j+dimensions[k][1], min_height - dimensions[k1][1] +1)):     #si dovrebbe capire graficamente                                                                                       
+                            for j1 in range(max(j-dimensions[k1][1]+1,0), min(j+dimensions[k][1], min_height - dimensions[k1][1] +1)):     #See the report                                                                                     
                                 s.add(Or(Not(X[k][j][i]), Not(X[k1][j1][i1])))
 
     #horizontal and vertical symmetry breaking constraint:
-##    s.add(lex_order(flatten(flatten(X)), flatten(flatten(hperm(X,width,dimensions)))))
-##    s.add(lex_order(flatten(flatten(X)), flatten(flatten(vperm(X,min_height,dimensions)))))
     s.add(lex_order(flatten(flatten(X)), flatten(flatten(hperm(X,width,dimensions))),'hsym'))
     s.add(lex_order(flatten(flatten(X)), flatten(flatten(vperm(X,min_height,dimensions))),'vsym'))
 
-    #possibili implied constraints:
-           #1) in ogni i,j ci può essere al più un'origine di un rettangolo k
-           #2) per ogni rettangolo k, Se X[k][j][i], allora i + dimensions[k][0] <= width
-           #3) analogo per l'altezza
     end_time=time.time()
     print('Model generated in', end_time - starting_time, 'seconds')
 
